f176aebe66f9693d21856dae5b9ff08dce078b32
schrob <robin.scheid@orbis.de>
2021-12-27 17:55:21 +0100

    Solved Day13

--- AdventOfCode21/Day_12.cs
+++ AdventOfCode21/Day_12.cs
@@ -39,35 +39,96 @@ namespace AdventOfCode21
             Stack<Cave> path = new();
             path.Push(caveSystem["start"]);
             int pathCount = 0;
-            FindPaths(path, ref pathCount, caveSystem["end"]) ;            
+            FindPaths_1(path, ref pathCount, caveSystem["end"]) ;            
             return new(pathCount.ToString());
         }
 
-        public void FindPaths(Stack<Cave> path, ref int pathCount, Cave end)
+        public void FindPaths_1(Stack<Cave> path, ref int pathCount, Cave end)
         {
-            foreach(var cave in path.Peek().ConnectedCaves)
+            foreach (var cave in path.Peek().ConnectedCaves)
             {
-                if(cave.IsSmall && path.Contains(cave))
+                if (cave.IsSmall && path.Contains(cave))
                 {
                     continue; // do not visit small cave again, discard this branching
                 }
-                if(cave.Name == "end")
+                if (cave.Name == end.Name)
                 {
                     pathCount++;
                     continue;
                 }
                 var newPath = new Stack<Cave>(path);
                 newPath.Push(cave);
-                FindPaths(newPath, ref pathCount, end);
+                FindPaths_1(newPath, ref pathCount, end);
             }
         }
 
         public override ValueTask<string> Solve_2()
         {
-            return new("");
+            Dictionary<string, Cave> caveSystem = new();
+            foreach (var line in input)
+            {
+                var split = line.Split('-');
+                string startCave = split[0];
+                string endCave = split[1];
+                if (!caveSystem.ContainsKey(startCave))
+                {
+                    caveSystem.Add(startCave, new Cave(startCave, Char.IsLower(startCave[0])));
+                }
+                if (!caveSystem.ContainsKey(endCave))
+                {
+                    caveSystem.Add(endCave, new Cave(endCave, Char.IsLower(endCave[0])));
+                }
+                caveSystem[startCave].AddConnectedCave(caveSystem[endCave]);
+                caveSystem[endCave].AddConnectedCave(caveSystem[startCave]);
+            }
+            Stack<Cave> path = new();
+            path.Push(caveSystem["start"]);
+            long pathCount = 0;
+            FindPaths_2(path, ref pathCount, false, caveSystem["end"]);
+            return new(pathCount.ToString());
         }
-    }
-    
+
+        public void FindPaths_2(Stack<Cave> path, ref long pathCount, bool twiceVisited, Cave end)
+        {
+            foreach (var cave in path.Peek().ConnectedCaves)
+            {
+                if (cave.Name == end.Name)
+                {
+                    pathCount++;
+                    continue;
+                }
+                if (cave.Name == "start")
+                {
+                    continue;
+                }
+                if (cave.IsSmall && path.Contains(cave))
+                {
+                    if(twiceVisited)
+                    {
+                        continue;
+                    }
+                    twiceVisited = true; // a small cave is now added to the path for the 2nd time
+                    var newPath1 = new Stack<Cave>(path);
+                    var newPath2 = new Stack<Cave>(path);
+                    newPath2.Push(cave);
+                    FindPaths_2(newPath2, ref pathCount, twiceVisited, end);
+                    continue;
+                }                
+                var newPath = new Stack<Cave>(path);
+                newPath.Push(cave);
+                FindPaths_2(newPath, ref pathCount, twiceVisited, end);
+            }
+        }
+
+        public void PrintPath(Stack<Cave> path)
+        {
+            while(path.Count > 0)
+            {
+                Console.Write(path.Pop().Name);
+            }
+            Console.WriteLine();
+        }
+    }    
 
     public class Cave
     {
---
+++ AdventOfCode21/Day_13.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoCHelper;
+
+namespace AdventOfCode21
+{
+    class Day_13 : BaseDay
+    {
+        private List<string> input;
+
+        public Day_13()
+        {
+            input = File.ReadAllLines(InputFilePath).ToList();
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            List<int> xValues = new();
+            List<int> yValues = new();
+            List<string> foldingInstructions = new();
+            foreach(var line in input)
+            {
+                if(line.StartsWith("fold"))
+                {
+                    foldingInstructions.Add(line.Split(' ')[2]);
+                } 
+                else if(line != "")
+                {
+                    xValues.Add(Int32.Parse(line.Split(',')[0]));
+                    yValues.Add(Int32.Parse(line.Split(',')[1]));
+                }                
+            }
+            bool[,] paper = new bool[xValues.Max() + 1, yValues.Max() + 1];
+            for(int i = 0; i < xValues.Count; i++)
+            {
+                paper[xValues[i], yValues[i]] = true;
+            }
+            var folded = Fold(paper, foldingInstructions[0]);
+            return new(CountDots(folded).ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            List<int> xValues = new();
+            List<int> yValues = new();
+            List<string> foldingInstructions = new();
+            foreach (var line in input)
+            {
+                if (line.StartsWith("fold"))
+                {
+                    foldingInstructions.Add(line.Split(' ')[2]);
+                }
+                else if (line != "")
+                {
+                    xValues.Add(Int32.Parse(line.Split(',')[0]));
+                    yValues.Add(Int32.Parse(line.Split(',')[1]));
+                }
+            }
+            bool[,] paper = new bool[xValues.Max() + 1, yValues.Max() + 1];
+            for (int i = 0; i < xValues.Count; i++)
+            {
+                paper[xValues[i], yValues[i]] = true;
+            }
+
+            foreach(var instruction in foldingInstructions)
+            {
+                paper = Fold(paper, instruction);
+                
+            }
+            //PrintCode(paper);
+            return new(CountDots(paper).ToString());
+        }
+
+        public bool[,] Fold(bool[,] paper, string instruction)
+        {
+            char direction = instruction.Split('=')[0][0];
+            int position = Int32.Parse(instruction.Split('=')[1]);
+            bool[,] newPaper = new bool[0,0];
+            if (direction == 'y')
+            {
+                for(int i = 1; i < paper.GetLength(1); i++)
+                {
+                    if(position - i < 0 || position + i >= paper.GetLength(1))
+                    {
+                        break;
+                    }
+                    for(int j = 0; j < paper.GetLength(0); j++)
+                    {                        
+                        paper[j, position - i] = paper[j, position - i] || paper[j, position + i];
+                    }
+                }
+                newPaper = new bool[paper.GetLength(0), position + 1];
+                for(int i = 0; i < paper.GetLength(0); i++)
+                {
+                    for (int j = 0; j <= position; j++)
+                    {
+                        newPaper[i, j] = paper[i, j];
+                    }
+                }
+            }
+            else
+            {
+                for(int i = 1; i < paper.GetLength(0); i++)
+                {
+                    if (position - i < 0 || position + i >= paper.GetLength(0))
+                    {
+                        break;
+                    }
+                    for (int j = 0; j < paper.GetLength(1); j++)
+                    {
+                        paper[position - i, j] = paper[position - i,j] || paper[position + i,j];
+                    }
+                }
+                newPaper = new bool[position + 1, paper.GetLength(1)];
+                for (int i = 0; i <= position; i++)
+                {
+                    for (int j = 0; j < paper.GetLength(1); j++)
+                    {
+                        newPaper[i, j] = paper[i, j];
+                    }
+                }
+            }
+            return newPaper;
+        }
+
+        public int CountDots(bool[,] paper)
+        {
+            int count = 0;
+            for(int i = 0; i < paper.GetLength(0); i++)
+            {
+                for(int j = 0; j < paper.GetLength(1); j++)
+                {
+                    if (paper[i, j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public void PrintCode(bool[,] paper)
+        {
+           
+            for(int i = 0; i < paper.GetLength(1); i++)
+            {
+                for(int j = 0; j < paper.GetLength(0); j++)
+                {
+                    if (paper[j,i])
+                    {
+                        Console.Write('#');
+                    }
+                    else
+                    {
+                        Console.Write('.');
+                    }
+                }
+                Console.WriteLine(); ;
+            }
+        }
+    }
+}
